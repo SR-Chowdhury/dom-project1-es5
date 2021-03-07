9140625d793e3df458c755a3824cca789315e7ee
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-03-07 19:31:43 +0600

    dom project 1 done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,7 +5,7 @@ $ = selector => document.querySelector(selector);
 window.onload = () => {
 
     const taskField = $('#inputField');
-    const addTaskBtn = $('#addNoteBtn');
+    // const addTaskBtn = $('#addNoteBtn');
     const allTaskParent = $('#allTask');
 
     taskField.addEventListener('keypress', (event) => {
@@ -16,24 +16,35 @@ window.onload = () => {
         }
     });
 
+
     function cerateNewTask(task, allTaskParent) {
         let col = document.createElement('div');
-        col.className = 'col-sm-3 d-flex';
+        col.className = 'col-sm-3';
 
         let singleTask = document.createElement('div');
-        singleTask.className = 'single-task';
+        singleTask.className = 'single-task d-flex';
 
         let pDiv = document.createElement('p');
         pDiv.innerHTML = task;
+        singleTask.appendChild(pDiv);
 
         let span = document.createElement('span');
-        span.innerHTML = 'X';
-        span.style.color = 'red';
+        span.innerHTML = '<i class="far fa-window-close"></i>';
+        span.style.color = '#fff';
         span.style.marginLeft = 'auto';
+        span.style.paddingLeft = '3px';
         span.style.cursor = 'pointer'
 
-        pDiv.appendChild(span);
-        singleTask.appendChild(pDiv);
+        singleTask.appendChild(span);
+
+        let taskController =   createTaskController(singleTask);
+        taskController.style.visibility = 'hidden';
+        singleTask.appendChild(taskController);
+
+        singleTask.onmouseenter = () => taskController.style.visibility = 'visible';
+        singleTask.onmouseleave = () => taskController.style.visibility = 'hidden';
+
+
         col.appendChild(singleTask);
         allTaskParent.appendChild(col);
         
@@ -41,6 +52,76 @@ window.onload = () => {
         span.addEventListener('click', () => {
             allTaskParent.removeChild(col);
         });
+
+    }
+
+    function createTaskController(singleTask) {
+
+        let controlPanel = document.createElement('div');
+        controlPanel.className = 'task-control-panel d-flex align-items-center';
+
+        let colorPalte = createColorPalte(singleTask);
+        controlPanel.appendChild(colorPalte); 
+
+        let editBtn = createEditBtn(singleTask);
+        controlPanel.appendChild(editBtn);
+
+        return controlPanel;
+    }
+
+    function createColorPalte(singleTask) {
+        let colors = ['palegreen', 'blue', 'yellow', 'purple', 'orange', 'gray'];
+
+        let colorDiv = document.createElement('div');
+        colorDiv.className = 'd-flex';
+
+        colors.forEach(color => {
+            let div = document.createElement('div');
+            div.className = 'color-circle ml-2';
+            div.style.background = color;
+            div.addEventListener('click', () => {
+                singleTask.style.background = color;
+            })
+            colorDiv.appendChild(div);  
+        });
+        return colorDiv;
+    }
+
+    function createEditBtn(singleTask) {
+
+        let editSpan = document.createElement('span');
+        editSpan.className = 'ml-auto mr-2';
+        editSpan.innerHTML = '<i class="far fa-edit"></i>';
+
+        editSpan.addEventListener('click', () => {
+            
+            let pTag = singleTask.querySelector('p');
+            let textArea = document.createElement('textarea');
+            textArea.className = 'inner-textarea';
+            textArea.style.height = singleTask.offsetHeight + 'px';
+            textArea.style.width = singleTask.offsetWidth + 'px';
+            textArea.innerHTML = pTag.innerHTML;
+
+            textArea.addEventListener('keypress', function(event) {
+
+                if(event.keyCode === 13) {
+                    event.stopPropagation();
+
+                    if(textArea.value) { // here this.value means textArea.value
+                        pTag.innerHTML = this.value;
+                        singleTask.removeChild(this); // here this means textArea
+                    }
+                    else {
+                        alert("please write someting");
+                    }
+                }
+            })
+
+            singleTask.appendChild(textArea);
+            
+        })
+
+        return editSpan;
 
     }
 }
